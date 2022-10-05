7eb1aa9f13353290a4e47966551ba1b3d9bfa422
Tarasov ILya <ilyatarasov75@gmail.com>
2022-10-05 23:58:30 +0300

    kmkl

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+<link rel="icon" type="image/png" href="../assets/img/favicon.png">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+<title>
+WebApp
+</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+ <body class="">
+  <div class="main-panel">
+    @include('includes.header')
+    @include('index')
+    @include('includes.footer')
+</div>
+<!-- Core JS Files -->
+</body>
+
+</html>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/bootstrap/bootstrap.css', 'resources/js/bootstrap/bootstrap.bundle.js'],
+            input: [
+                "resources/css/bootstrap/bootstrap.css",
+                "resources/js/bootstrap/bootstrap.bundle.min.js",
+                "resources/js/jquery.min.js"
+            ],
             refresh: true,
         }),
     ],
