f4bf850d095a04c84014d8941fbb89462b552535
Tarasov ILya <ilyatarasov75@gmail.com>
2022-10-22 09:40:10 +0300

    delete useless bootstap files

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/bootstrap/bootstrap.css",
+                "resources/css/bootstrap/bootstrap.min.css",
                 "resources/js/bootstrap/bootstrap.bundle.min.js",
-                "resources/js/jquery.min.js"
             ],
             refresh: true,
         }),
