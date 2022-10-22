16daeadb81666e2777366e9bef5bcf235061f4eb
Tarasov ILya <ilyatarasov75@gmail.com>
2022-10-22 08:00:59 +0300

    layouts

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -58,7 +58,7 @@
         </div>
     </div>
 </header>
-<div class="d-flex flex-wrap justify-content-center bg-light py-3 mb-4 border-bottom fixed-top">
+{{-- <div class="d-flex flex-wrap justify-content-center bg-light py-3 mb-4 border-bottom fixed-top">
     <a href="/" class="d-flex align-items-center  mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32">
             <use xlink:href="#bootstrap"></use>
@@ -73,4 +73,4 @@
         <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
         <li class="nav-item"><a href="#" class="nav-link">About</a></li>
     </ul>
-</div>
+</div> --}}
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 
 <head>
-<meta charset="utf-8" />
-<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-<link rel="icon" type="image/png" href="../assets/img/favicon.png">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-<title>
-WebApp
-</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+        name='viewport' />
+    <title>
+        WebApp
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
- <body class="">
-  <div class="main-panel">
+<body class="">
     @include('includes.header')
-    @include('index')
+    @yield('content')
     @include('includes.footer')
-</div>
-<!-- Core JS Files -->
+    <!-- Core JS Files -->
 </body>
 
 </html>
