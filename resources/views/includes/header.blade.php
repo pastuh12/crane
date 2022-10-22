d957b41858c3b72b8a6c2da4ece52c5a72cb72b2
Tarasov ILya <ilyatarasov75@gmail.com>
2022-10-22 09:39:43 +0300

    fix default components

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <header class="d-flex flex-column">
-    <div class="d-flex flex-wrap justify-content-center  py-3 mb-2 border-bottom">
+    <div class="d-flex flex-wrap justify-content-center  py-3  mb-5  border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32">
                 <use xlink:href="#bootstrap"></use>
@@ -8,57 +8,14 @@
         </a>
 
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Главная</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Техника</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">О нас</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Контакты</a></li>
         </ul>
     </div>
-    <div class=" pb-2 mb-2 d-flex justify-content-around border-bottom">
-        <div class="btn-group" role="group">
-            <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Dropdown
-            </button>
-            <ul class="dropdown-menu" style="">
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-            </ul>
-        </div>
-        <div class="btn-group" role="group">
-            <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Dropdown
-            </button>
-            <ul class="dropdown-menu" style="">
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-            </ul>
-        </div>
-        <div class="btn-group" role="group">
-            <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Dropdown
-            </button>
-            <ul class="dropdown-menu" style="">
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-            </ul>
-        </div>
-        <div class="btn-group" role="group">
-            <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Dropdown
-            </button>
-            <ul class="dropdown-menu" style="">
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-            </ul>
-        </div>
-    </div>
 </header>
-{{-- <div class="d-flex flex-wrap justify-content-center bg-light py-3 mb-4 border-bottom fixed-top">
+<div class="d-flex flex-wrap justify-content-center bg-white py-3 border-bottom fixed-top">
     <a href="/" class="d-flex align-items-center  mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32">
             <use xlink:href="#bootstrap"></use>
@@ -67,10 +24,9 @@
     </a>
 
     <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="{{route('home')}}" class="nav-link" aria-current="page">Главная</a></li>
+        <li class="nav-item"><a href="{{route('cranes')}}" class="nav-link">Техника</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">О нас</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Контакты</a></li>
     </ul>
-</div> --}}
+</div>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,12 +11,11 @@
     <title>
         WebApp
     </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="./build/assets/bootstrap.min.f066aefa.css" rel="stylesheet">
+    <script src="./build/assets/bootstrap.bundle.min.77fb9fc3.js" defer></script>
 </head>
 
-<body class="">
+<body class="bg-light">
     @include('includes.header')
     @yield('content')
     @include('includes.footer')
